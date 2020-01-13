35b83c35cb7161439bfe55b422c59381e165ee41
Igor Sudenko <sudenkoweb@gmail.com>
2020-01-13 21:33:59 +0200

    add Create Component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import {HeaderComponent} from './components/header.component';
 import {NavigationComponent} from "./components/navigation.component";
+import {CreateComponent} from "./components/create.component";
 
 new HeaderComponent('header');
 new NavigationComponent('navigation');
+new CreateComponent('create');
