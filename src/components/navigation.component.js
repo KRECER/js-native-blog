a30a568b38b3b124622474c841721e1a9a100b50
Igor Sudenko <sudenkoweb@gmail.com>
2020-01-13 21:35:17 +0200

    refactoring

--- src/components/navigation.component.js
+++ src/components/navigation.component.js
@@ -1,40 +1,39 @@
 import {Component} from "../core/component";
 
+const showTabItem = (name) => {
+  const $tab = document.getElementById(name);
+  $tab.classList.remove('hide');
+};
+
+const hideTabItem = (name) => {
+  const $tab = document.getElementById(name);
+  $tab.classList.add('hide');
+};
+
+const onClickTabLink = function(event){
+  event.preventDefault();
+  const target = event.target;
+
+  if (target.classList.contains('tab')) {
+    const prevActiveTabLink = this.$el.querySelector('.active');
+    const prevActiveTabItem = prevActiveTabLink.dataset.name;
+    const activeTabLink = target;
+    const activeTabItem = activeTabLink.dataset.name;
+
+    prevActiveTabLink.classList.remove('active');
+    activeTabLink.classList.add('active');
+    hideTabItem(prevActiveTabItem);
+    showTabItem(activeTabItem);
+  }
+};
+
 class NavigationComponent extends Component {
   constructor(id) {
     super(id);
-    this.init();
   }
 
-  #showTabItem = (name) => {
-    const $tab = document.getElementById(name);
-    $tab.classList.remove('hide');
-  };
-
-  #hideTabItem = (name) => {
-    const $tab = document.getElementById(name);
-    $tab.classList.add('hide');
-  };
-
-  #onClickTabLink = (event) => {
-    event.preventDefault();
-    const target = event.target;
-
-    if (target.classList.contains('tab')) {
-      const prevActiveTabLink = this.$el.querySelector('.active');
-      const prevActiveTabItem = prevActiveTabLink.dataset.name;
-      const activeTabLink = target;
-      const activeTabItem = activeTabLink.dataset.name;
-
-      prevActiveTabLink.classList.remove('active');
-      activeTabLink.classList.add('active');
-      this.#hideTabItem(prevActiveTabItem);
-      this.#showTabItem(activeTabItem);
-    }
-  };
-
   init() {
-    this.$el.addEventListener('click', this.#onClickTabLink);
+    this.$el.addEventListener('click', onClickTabLink.bind(this));
   }
 }
 
