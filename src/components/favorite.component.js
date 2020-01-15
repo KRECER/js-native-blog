7087e18893d9e4c8cf4c7ea76f63a28be79a6d5b
Igor Sudenko <sudenkoweb@gmail.com>
2020-01-15 18:38:06 +0200

    favorite.component: добавил;

---
+++ src/components/favorite.component.js
@@ -0,0 +1,13 @@
+import {Component} from "../core/component";
+
+class FavoriteComponent extends Component {
+  constructor(id) {
+    super(id);
+  }
+
+  init() {
+
+  }
+}
+
+export {FavoriteComponent};
