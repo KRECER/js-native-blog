64c763f2dce65905c9bef8c1f403a761d72722dc
Igor Sudenko <sudenkoweb@gmail.com>
2020-01-16 20:13:44 +0200

    create.component: рефакторинг;

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -3,16 +3,6 @@ import {Form} from "../core/form";
 import {Validators} from "../core/validators";
 import {ApiService} from "../services/api.service";
 
-const onSubmit = function(event) {
-  event.preventDefault();
-
-  if (this.form.isValid) {
-    const formData = {type: this.$el.type.value, date: new Date().toLocaleDateString(), ...this.form.getValuesFromControls()};
-    ApiService.createPost(formData);
-    this.$el.reset();
-  }
-};
-
 class CreateComponent extends Component {
   constructor(id) {
     super(id);
@@ -23,5 +13,15 @@ class CreateComponent extends Component {
     this.form = new Form(this.$el, {title: [Validators.required], fulltext: [Validators.required, Validators.minLength(8)]});
   }
 }
+
+const onSubmit = function(event) {
+  event.preventDefault();
+
+  if (this.form.isValid) {
+    const formData = {type: this.$el.type.value, date: new Date().toLocaleDateString(), ...this.form.getValuesFromControls()};
+    ApiService.createPost(formData);
+    this.$el.reset();
+  }
+};
 
 export {CreateComponent};
