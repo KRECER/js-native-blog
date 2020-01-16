6ad2f4ee146bd7bc6df11e9fb34b66b58e9338c0
Igor Sudenko <sudenkoweb@gmail.com>
2020-01-16 20:30:20 +0200

    navigation-factory: добавил имя компонента в инстанс;

--- src/components/navigation-factory.js
+++ src/components/navigation-factory.js
@@ -1,24 +1,28 @@
 import {PostsComponent} from "./posts.component";
 import {CreateComponent} from "./create.component";
 import {FavoriteComponent} from "./favorite.component";
+import {Preloader} from "./preloader.component";
 
-class Factory {
+class NavigationFactory {
   static create(name) {
     let instance = {};
 
     switch(name) {
       case 'posts':
-        instance = new PostsComponent('posts');
+        instance = new PostsComponent('posts', {preloader: new Preloader('loader')});
+        instance.name = 'posts';
         break;
       case 'create':
         instance = new CreateComponent('create');
+        instance.name = 'create';
         break;
       case 'favorite':
-        instance = new FavoriteComponent('favorite');
+        instance = new FavoriteComponent('favorite', {preloader: new Preloader('loader')});
+        instance.name = 'favorite';
         break;
     }
     return instance;
   }
 }
 
-export {Factory};
+export {NavigationFactory};
