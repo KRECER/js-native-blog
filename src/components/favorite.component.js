e7eee17eb2682bcd707d500f2b8b9b5e78696a22
Igor Sudenko <sudenkoweb@gmail.com>
2020-01-16 20:25:27 +0200

    favorite.component: добавил вывод списка избранного;

--- src/components/favorite.component.js
+++ src/components/favorite.component.js
@@ -1,12 +1,41 @@
 import {Component} from "../core/component";
+import {Template} from "../core/template";
+import {ApiService} from "../services/api.service";
 
 class FavoriteComponent extends Component {
-  constructor(id) {
+  constructor(id, {preloader}) {
     super(id);
+    this.preloader = preloader;
   }
 
   init() {
+    this.$el.addEventListener('click', linkClickHandler.bind(this));
+  }
 
+  onShow() {
+    const favorites = JSON.parse(localStorage.getItem('favorites'));
+    const source = document.querySelector('#favorites-template').innerHTML;
+    const render = Template.compile(source);
+    this.$el.innerHTML = render(favorites);
+  }
+
+  onHide() {
+    this.$el.innerHTML = '';
+  }
+}
+
+async function linkClickHandler(event) {
+  event.preventDefault();
+
+  if (event.target.classList.contains('js-favorite-link')) {
+    this.$el.innerHTML = '';
+    this.preloader.show();
+    const postId = event.target.dataset.postId;
+    const post = await ApiService.fetchPostById(postId);
+    this.preloader.hide();
+    const source = document.getElementById('post-template').innerHTML;
+    const render = Template.compile(source);
+    this.$el.innerHTML = render([post]);
   }
 }
 
